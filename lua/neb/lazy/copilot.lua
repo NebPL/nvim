ac16b7d0563b1ed643ba31ffac53332dbc98ba3f
NebPL <160783369+NebPL@users.noreply.github.com>
2025-05-20 19:54:31 +0200

    copilot

---
+++ lua/neb/lazy/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "github/copilot.vim",
+  event = "InsertEnter", -- nur laden wenn man in den Insert-Modus geht
+  config = function()
+    vim.g.copilot_no_tab_map = true -- verhindert Tab-Akzeptieren
+    vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ""
+
+    -- Mappe <leader>v zum Akzeptieren des Vorschlags
+    vim.api.nvim_set_keymap("i", "<leader>v", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+  end,
+}
+
