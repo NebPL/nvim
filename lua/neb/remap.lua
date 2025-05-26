208931816c65a95520e4c36c867cf149874301cf
NebPL <160783369+NebPL@users.noreply.github.com>
2025-05-26 09:24:39 +0200

    Faster Space

--- lua/neb/remap.lua
+++ lua/neb/remap.lua
@@ -1,25 +1,20 @@
+-- 1) Leader festlegen
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>')
+-- 2) Leader-Lag minimieren
+vim.opt.timeout = true
+vim.opt.timeoutlen = 100 -- 0,3 s Wartedauer
 
+-- 3) Reinen Space von der Leader-Logik abkoppeln
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- 4) Deine bisherigen Maps
+vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>', { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Gehe zum Anfang der aktuellen Zeile mit 'r'
-vim.api.nvim_set_keymap('n', 'r', '^', { noremap = true, silent = true })
-
--- Gehe zum Ende der aktuellen Zeile mit 't'
-vim.api.nvim_set_keymap('n', 't', '$', { noremap = true, silent = true })
-
-
-vim.api.nvim_set_keymap('n', 'no', ':noh<CR>', { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('n', '<leader>e', ':Oil<CR>', { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>mc", function()
-  vim.cmd("MultipleCursorsFind")
-end, { desc = "Multiple cursors starten" })
-
-
-
+vim.keymap.set('n', 'r', '^', { noremap = true, silent = true })
+vim.keymap.set('n', 't', '$', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', ':Oil<CR>', { silent = true })
+vim.keymap.set('n', '<leader>mc', function() vim.cmd("MultipleCursorsFind") end,
+	{ desc = "Multiple cursors starten" })
